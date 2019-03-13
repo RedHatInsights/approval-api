2f45d50191f4454051bdc05eceaec492e077c902
hsong-rh <hsong@redhat.com>
2019-03-13 16:34:28 -0400

    Changed based on comments

--- app/models/concerns/convertable.rb
+++ app/models/concerns/convertable.rb
@@ -4,8 +4,8 @@ module Convertable
   def convert_date_id(attributes_hash)
     attributes_hash.tap do |hash|
       attributes_hash.each_key do |attr_key|
-        hash[attr_key] = send(attr_key.to_sym).iso8601 if attribute(attr_key).kind_of?(Time) && send(attr_key.to_sym)
-        hash[attr_key] = send(attr_key.to_sym).to_s if attr_key.ends_with?("_id") || attr_key == "id"
+        hash[attr_key] = send(attr_key).iso8601 if attribute(attr_key).kind_of?(Time) && respond_to?(attr_key)
+        hash[attr_key] = send(attr_key).to_s if attr_key.ends_with?("_id") || attr_key == "id"
       end
     end
   end
