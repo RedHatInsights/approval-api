515983e5aeba51ee1d6b5d2a93e4b85a4e73ec40
hsong-rh <hsong@redhat.com>
2019-03-29 16:46:45 -0400

    Allow no requester is set in request creation

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -2,7 +2,6 @@ RSpec.describe Request, type: :model do
   it { should belong_to(:workflow) }
   it { should have_many(:stages) }
 
-  it { should validate_presence_of(:requester) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:content) }
 end
