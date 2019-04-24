742c03dd0db4597d2c2ce6c0ff594402fee773dd
hsong-rh <hsong@redhat.com>
2019-04-24 15:48:22 -0400

    Enabled to list requests by approver's username

--- spec/controllers/v1.0/requests_controller_spec.rb
+++ spec/controllers/v1.0/requests_controller_spec.rb
@@ -4,9 +4,17 @@ RSpec.describe Api::V1x0::RequestsController, :type => :request do
   let(:request_header) { { 'x-rh-identity' => encoded_user } }
   let(:tenant) { create(:tenant, :external_tenant => 369_233) }
 
+  let(:username_1) { "jdoe@acme.com" }
+  let(:username_2) { "john@acme.com" }
+  let(:username_3) { "joe@acme.com" }
+  let(:group1) { double(:name => 'group1', :uuid => "123") }
+  let(:group2) { double(:name => 'group2', :uuid => "234") }
+
   let!(:template) { create(:template) }
-  let!(:workflow) { create(:workflow, :name => 'Test always approve') } #:template_id => template.id) }
+  let!(:workflow) { create(:workflow, :name => 'Always approve', :group_refs => [group1.uuid, group2.uuid]) }
   let(:workflow_id) { workflow.id }
+  let!(:workflow_2) { create(:workflow, :name => 'workflow_2', :group_refs => [group2.uuid]) }
+  let!(:workflow_3) { create(:workflow, :name => 'workflow_3', :group_refs => [group1.uuid]) }
   let!(:requests) do
     ManageIQ::API::Common::Request.with_request(:headers => request_header, :original_url => "localhost/approval") do
       create_list(:request, 2, :workflow_id => workflow.id, :tenant_id => tenant.id)
@@ -15,8 +23,16 @@ RSpec.describe Api::V1x0::RequestsController, :type => :request do
   let(:id) { requests.first.id }
   let!(:requests_with_same_state) { create_list(:request, 2, :state => 'notified', :workflow_id => workflow.id, :tenant_id => tenant.id) }
   let!(:requests_with_same_decision) { create_list(:request, 2, :decision => 'approved', :workflow_id => workflow.id, :tenant_id => tenant.id) }
+  let!(:requests_with_workflow_2) { create_list(:request, 3, :workflow_id => workflow_2.id, :tenant_id => tenant.id) }
+  let!(:requests_with_workflow_3) { create_list(:request, 4, :workflow_id => workflow_3.id, :tenant_id => tenant.id) }
 
   let(:api_version) { version }
+
+  before do
+    allow(Group).to receive(:all).with(username_1).and_return([group1, group2])
+    allow(Group).to receive(:all).with(username_2).and_return([group2])
+    allow(Group).to receive(:all).with(username_3).and_return([group1])
+  end
 
   # Test suite for GET /workflows/:workflow_id/requests
   describe 'GET /workflows/:workflow_id/requests' do
@@ -55,7 +71,7 @@ RSpec.describe Api::V1x0::RequestsController, :type => :request do
     it 'returns requests' do
       expect(json['links']).not_to be_empty
       expect(json['links']['first']).to match(/limit=5&offset=0/)
-      expect(json['links']['last']).to match(/limit=5&offset=5/)
+      expect(json['links']['last']).to match(/limit=5&offset=10/)
       expect(json['data'].size).to eq(5)
     end
 
@@ -113,6 +129,43 @@ RSpec.describe Api::V1x0::RequestsController, :type => :request do
 
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
+    end
+  end
+
+  # Test suite for GET /requests?approver=
+  describe 'GET /requests?approver=jode@acme.com' do
+    before { get "#{api_version}/requests?approver=#{username_1}", :headers => request_header }
+
+    it 'return requests' do
+      expect(json['links']).not_to be_empty
+      expect(json['links']['first']).to match(/offset=0/)
+      expect(json['data'].size).to eq(13)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  # Test suite for GET /requests?approver=
+  describe 'GET /requests?approver=john@acme.com' do
+    before { get "#{api_version}/requests?approver=#{username_2}", :headers => request_header }
+
+    it 'return requests' do
+      expect(json['links']).not_to be_empty
+      expect(json['links']['first']).to match(/offset=0/)
+      expect(json['data'].size).to eq(9)
+    end
+  end
+
+  # Test suite for GET /requests?approver=
+  describe 'GET /requests?approver=joe@acme.com' do
+    before { get "#{api_version}/requests?approver=#{username_3}", :headers => request_header }
+
+    it 'return requests' do
+      expect(json['links']).not_to be_empty
+      expect(json['links']['first']).to match(/offset=0/)
+      expect(json['data'].size).to eq(10)
     end
   end
 
