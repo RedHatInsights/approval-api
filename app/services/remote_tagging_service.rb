29083116398cd34be057ed7d0960c86ed2b102ac
Madhu Kanoor <mkanoor@redhat.com>
2019-11-01 16:04:35 -0400

    Fixed rubocop warnings

--- app/services/remote_tagging_service.rb
+++ app/services/remote_tagging_service.rb
@@ -11,31 +11,30 @@ class RemoteTaggingService
     self
   end
 
-
   def self.remotes
-    [{ :app_name => 'topology', :object_type => 'ServiceInventory', :url => proc {topo_url} },
-     { :app_name => 'topology', :object_type => 'Credential',       :url => proc {topo_url} },
-     { :app_name => 'catalog',  :object_type => 'PortfolioItem',    :url => proc {catalog_url} },
-     { :app_name => 'catalog',  :object_type => 'Portfolio',        :url => proc {catalog_url} },
-     { :app_name => 'sources',  :object_type => 'Source',           :url => proc {sources_url} }]
+    [{ :app_name => 'topology', :object_type => 'ServiceInventory', :url => proc { topo_url } },
+     { :app_name => 'topology', :object_type => 'Credential',       :url => proc { topo_url } },
+     { :app_name => 'catalog',  :object_type => 'PortfolioItem',    :url => proc { catalog_url } },
+     { :app_name => 'catalog',  :object_type => 'Portfolio',        :url => proc { catalog_url } },
+     { :app_name => 'sources',  :object_type => 'Source',           :url => proc { sources_url } }]
   end
 
   private
 
   def self.topo_url
-    url = ENV.fetch('TOPOLOGICAL_INVENTORY_URL') { raise 'TOPOLOGICAL_INVENTORY_URL is not set'}
+    url = ENV.fetch('TOPOLOGICAL_INVENTORY_URL') { raise 'TOPOLOGICAL_INVENTORY_URL is not set' }
     "#{url}/api/topological-inventory/v1.0"
   end
   private_class_method :topo_url
 
   def self.catalog_url
-    url = ENV.fetch('CATALOG_URL') { raise 'CATALOG_URL is not set'}
+    url = ENV.fetch('CATALOG_URL') { raise 'CATALOG_URL is not set' }
     "#{url}/api/catalog/v1.0"
   end
   private_class_method :catalog_url
 
   def self.sources_url
-    url = ENV.fetch('SOURCES_URL') { raise 'SOURCES_URL is not set'}
+    url = ENV.fetch('SOURCES_URL') { raise 'SOURCES_URL is not set' }
     "#{url}/api/sources/v1.0"
   end
   private_class_method :sources_url
