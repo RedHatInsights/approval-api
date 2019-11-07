e82bbde578b6a51410acabc9b0df07a759bb8d6a
Madhu Kanoor <mkanoor@redhat.com>
2019-11-07 15:53:45 -0500

    Fetch sorted workflows based on TagResources
    Catalog would be sending a collection of TagResources and this
    code will return a list of workflows sorted by the sequence number

--- spec/services/workflow_find_service_spec.rb
+++ spec/services/workflow_find_service_spec.rb
@@ -30,4 +30,23 @@ RSpec.describe WorkflowFindService do
       expect(another_workflows).to eq([])
     end
   end
+
+  describe 'find_by_tag_resources' do
+    include_context "tag_resource_objects"
+
+    it 'finds sorted workflow based on tags' do
+      workflows = subject.find_by_tag_resources([tag_resource1])
+      expect(workflows).to eq([workflow2, workflow1])
+    end
+
+    it 'if tags is empty returns empty array' do
+      workflows = subject.find_by_tag_resources([tagless_resource])
+      expect(workflows).to be_empty
+    end
+
+    it 'if input is empty array return empty array' do
+      workflows = subject.find_by_tag_resources([])
+      expect(workflows).to be_empty
+    end
+  end
 end
