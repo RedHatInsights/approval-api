d3ab5c5130c51440abd563c86984dec66cc049c3
Bill Wei <bilwei@redhat.com>
2019-12-05 17:56:19 -0500

    Fix bypassing jbpm logic

--- app/services/request_update_service.rb
+++ app/services/request_update_service.rb
@@ -125,7 +125,7 @@ class RequestUpdateService
 
   # start the external approval process if configured
   def start_request
-    return unless !bypass? || request.workflow.try(:external_processing?)
+    return unless !bypass? && request.workflow.try(:external_processing?)
 
     template = request.workflow.template
     processor_class = "#{template.process_setting['processor_type']}_process_service".classify.constantize
@@ -141,7 +141,7 @@ class RequestUpdateService
 
   # complete the external approval process if configured
   def finish_request(decision)
-    return unless !bypass? || request.workflow.try(:external_processing?)
+    return unless !bypass? && request.workflow.try(:external_processing?)
 
     template = request.workflow.template
     processor_class = "#{template.signal_setting['processor_type']}_process_service".classify.constantize
