bc625f02c7ef086f9c3c1f76092524eebfe6ed9d
Madhu Kanoor <mkanoor@redhat.com>
2020-01-20 10:24:44 -0500

    openapi.json can be retrieved without credentials
    https://projects.engineering.redhat.com/browse/SSP-1145
    
    This makes it consistent with topology, sources and catalog
    There was an earlier PR https://github.com/RedHatInsights/approval-api/pull/121
    which was closed without merging.

--- spec/controllers/v1.0/root_controller_spec.rb
+++ spec/controllers/v1.0/root_controller_spec.rb
@@ -1,18 +1,17 @@
 RSpec.describe Api::V1x0::RootController, :type => :request do
   let(:encoded_user) { encoded_user_hash }
-  let(:request_header) { { 'x-rh-identity' => encoded_user } }
   let(:api_version) { version }
 
   context "v1.0" do
     it "#openapi.json" do
-      get "#{api_version}/openapi.json", :headers => request_header
+      get "#{api_version}/openapi.json"
 
       expect(response.content_type).to eq("application/json")
       expect(response).to have_http_status(:ok)
     end
 
     it "redirects properly" do
-      get "#{api_version.split('.').first}/openapi.json", :headers => request_header
+      get "#{api_version.split('.').first}/openapi.json"
 
       expect(response.status).to eq(302)
       expect(response.headers["Location"]).to eq("#{api_version}/openapi.json")
