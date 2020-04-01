015a5a6d7f1df2cca67836a5f3f57e3eae2e9e58
Bill Wei <bilwei@redhat.com>
2020-04-01 11:06:46 -0400

    Permission based rbac for workflows controller 1.2

--- app/controllers/api/v1x2/workflows_controller.rb
+++ app/controllers/api/v1x2/workflows_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1x2
     class WorkflowsController < Api::V1x0::WorkflowsController
       def show
-        raise Exceptions::NotAuthorizedError, "Not Authorized for workflows" if Insights::API::Common::RBAC::Access.enabled? && !admin?
+        authorize Workflow.find(params.require(:id))
 
         json_response(WorkflowGetService.new(params.require(:id)).get)
       end
