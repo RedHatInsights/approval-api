63f83388e4d1ba1fe7c3560b8a178aaa4af11420
hsong-rh <hsong@redhat.com>
2020-05-05 11:16:56 -0400

    Drop unused AccessControlEntry table

--- app/models/workflow.rb
+++ app/models/workflow.rb
@@ -8,7 +8,6 @@ class Workflow < ApplicationRecord
   belongs_to :template
   has_many :requests, -> { order(:id => :asc) }, :inverse_of => :workflow
   has_many :tag_links, :dependent => :destroy, :inverse_of => :workflow
-  has_many :access_control_entries, :as => :aceable, :dependent => :destroy, :inverse_of => :aceable
 
   validates :name, :presence => true, :uniqueness => {:scope => :tenant}
 
