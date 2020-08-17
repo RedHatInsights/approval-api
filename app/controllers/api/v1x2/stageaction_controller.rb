f32d861e5fde1102b4a0dbc4fd5ef7246fef645f
Eric Winchell <ewinchel@redhat.com>
2020-08-17 12:25:48 -0400

    Add status label to results screen

--- app/controllers/api/v1x2/stageaction_controller.rb
+++ app/controllers/api/v1x2/stageaction_controller.rb
@@ -79,6 +79,7 @@ module Api
         {
           :orderer       => request.requester_name,
           :orderer_email => request.owner,
+          :status        => request.decision,
           :product       => request.content["product"],
           :portfolio     => request.content["portfolio"],
           :order_id      => request.content["order_id"],
