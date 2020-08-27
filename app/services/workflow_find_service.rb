340ebadd96e15555462d7a3fb0c32fd7623d98bb
Bill Wei <bilwei@redhat.com>
2020-08-27 10:55:33 -0400

    Replace pluck by select to combine two sql statements to one

--- app/services/workflow_find_service.rb
+++ app/services/workflow_find_service.rb
@@ -2,7 +2,7 @@ class WorkflowFindService
   # find workflows for a remote resource with app_name, object_type, and object_id
   def find(tag_attrs)
     tags = GetRemoteTags.new(tag_attrs).process.tags
-    workflow_ids = TagLink.where(tag_attrs.except(:object_id)).where(:tag_name => tags).pluck(:workflow_id)
+    workflow_ids = TagLink.where(tag_attrs.except(:object_id)).where(:tag_name => tags).select(:workflow_id)
 
     Workflow.where(:id => workflow_ids)
   end
